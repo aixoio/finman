daf03d096b0d3b2dec7067f1a686477bd30d1a4f
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-08-07 15:32:04 +1000

    add current with 70%

--- src/routes/SidebarLink.svelte
+++ src/routes/SidebarLink.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { page } from "$app/state";
     import type { Snippet } from "svelte";
 
     interface Props {
@@ -12,9 +13,18 @@
 
 <a
     {href}
-    class="flex cursor-pointer items-center gap-2 rounded px-2 py-0.5 hover:bg-foreground/5 active:bg-foreground/20"
+    class="flex cursor-pointer items-center gap-2 rounded px-2 py-0.5 hover:bg-foreground/5 active:bg-foreground/20 select-none"
+    class:current={page.url.pathname == href}
 >
     {@render icon()}
     <div class="w-full"></div>
     {@render children()}
 </a>
+
+<style lang="postcss">
+    @reference "tailwindcss";
+
+    .current {
+        @apply opacity-70;
+    }
+</style>
