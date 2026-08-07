986eac6cb188f1fa1861eebb23dd2698c4fb577d
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-08-07 15:19:51 +1000

    add license link

--- src/routes/Sidebar.svelte
+++ src/routes/Sidebar.svelte
@@ -25,5 +25,21 @@
             {/snippet}
             <span>home</span>
         </SidebarLink>
+        <SidebarLink href="/license">
+            {#snippet icon()}
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    class="bi bi-house w-8 h-8"
+                    viewBox="0 0 16 16"
+                >
+                    <path
+                        d="M0 8a4 4 0 0 1 7.465-2H14a.5.5 0 0 1 .354.146l1.5 1.5a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0L13 9.207l-.646.647a.5.5 0 0 1-.708 0L11 9.207l-.646.647a.5.5 0 0 1-.708 0L9 9.207l-.646.647A.5.5 0 0 1 8 10h-.535A4 4 0 0 1 0 8m4-3a3 3 0 1 0 2.712 4.285A.5.5 0 0 1 7.163 9h.63l.853-.854a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.793-.793-1-1h-6.63a.5.5 0 0 1-.451-.285A3 3 0 0 0 4 5"
+                    />
+                    <path d="M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
+                </svg>
+            {/snippet}
+            <span>license</span>
+        </SidebarLink>
     </div>
 </aside>
