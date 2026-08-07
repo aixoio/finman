4850829f5698e781f2014b61c65b2074c33d229a
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-08-07 14:35:49 +1000

    add base for sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
     import "./layout.css";
+    import Sidebar from "./Sidebar.svelte";
 
     const { children } = $props();
 </script>
+
+<Sidebar></Sidebar>
 
 {@render children()}
---
+++ src/routes/Sidebar.svelte
@@ -0,0 +1,3 @@
+<div>
+    <h1>finman</h1>
+</div>
