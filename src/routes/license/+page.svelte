9d47ccbc625794154a7f72f2bb4b59d8a502b04f
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-08-07 15:42:03 +1000

    add license page with tailwind css typography

--- src/routes/license/+page.svelte
+++ src/routes/license/+page.svelte
@@ -1,1 +1,26 @@
-<h1>license</h1>
+<div class="m-4">
+    <div class="prose-invert prose">
+        <h1>license</h1>
+
+        <p>
+            This program is free software: you can redistribute it and/or modify
+            it under the terms of the GNU Affero General Public License as
+            published by the Free Software Foundation, either version 3 of the
+            License, or (at your option) any later version.
+        </p>
+
+        <p>
+            This program is distributed in the hope that it will be useful, but
+            WITHOUT ANY WARRANTY; without even the implied warranty of
+            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+            Affero General Public License for more details.
+        </p>
+        <p>
+            You should have received a copy of the GNU Affero General Public
+            License along with this program. If not, see
+            <a href="https://www.gnu.org/licenses/" target="_blank"
+                >https://www.gnu.org/licenses/</a
+            >.
+        </p>
+    </div>
+</div>
