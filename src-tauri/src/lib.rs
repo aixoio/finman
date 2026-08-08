cab65436080e061ccfcd80925173b716f69ebaa9
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-08-08 15:50:07 +1000

    add database as AppState

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,35 @@
+use std::fs;
+
+use tauri::Manager;
+
+use crate::database::{Database, DATABASE_FILENAME};
+
 mod database;
+
+struct AppState {
+    database: Database,
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let app_local_data_dir = app.path().app_local_data_dir()?;
+
+            fs::create_dir_all(&app_local_data_dir)?;
+
+            let db_path = app_local_data_dir.join(DATABASE_FILENAME);
+            let db_path = db_path.to_str().expect("cannot get db path");
+
+            let database =
+                tauri::async_runtime::block_on(async { Database::build(db_path).await })?;
+
+            let app_state = AppState { database };
+
+            app.manage(app_state);
+
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
