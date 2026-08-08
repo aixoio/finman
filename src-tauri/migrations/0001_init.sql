3b0aba21db39291ae5219549731929f680779018
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-08-08 15:27:31 +1000

    add first migration

---
+++ src-tauri/migrations/0001_init.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE items (
+    uuid TEXT PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    comment TEXT,
+    type TEXT NOT NULL,
+    target_cents INTEGER NOT NULL,
+    current_cents INTEGER NOT NULL,
+    archived BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TEXT NOT NULL,
+    updated_at TEXT NOT NULL
+);
