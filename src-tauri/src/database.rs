f441f8c04acf44af1786e922e2dcd157ec430e87
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-08-08 16:23:04 +1000

    add method to database struct database::select_all_items_not_archived

--- src-tauri/src/database.rs
+++ src-tauri/src/database.rs
@@ -1,18 +1,21 @@
-use std::str::FromStr;
+use std::{error::Error, str::FromStr};
 
+use serde::{Deserialize, Serialize};
 use sqlx::{
     sqlite::{SqliteConnectOptions, SqlitePoolOptions},
-    SqlitePool,
+    Row, SqlitePool,
 };
 
 pub const DATABASE_FILENAME: &str = "finman_database.sqlite3";
 
+#[derive(Debug, Serialize, Deserialize)]
 pub enum ItemType {
     Savings,
     SelfLoan,
     ExternalLoan,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Item {
     uuid: String,
     name: String,
@@ -39,5 +42,42 @@ impl Database {
         sqlx::migrate!().run(&pool).await?;
 
         Ok(Self { pool })
+    }
+
+    pub async fn select_all_items_not_archived(&self) -> Result<Vec<Item>, Box<dyn Error>> {
+        let rows = sqlx::query("SELECT * FROM items WHERE archived = FALSE")
+            .fetch_all(&self.pool)
+            .await?;
+
+        let mut items = Vec::new();
+
+        for row in rows {
+            let uuid = row.get("uuid");
+            let name = row.get("name");
+            let comment = row.get("comment");
+            let item_type: String = row.get("item_type");
+            let item_type: ItemType = serde_json::from_str(&item_type)?;
+            let target_cents = row.get("target_cents");
+            let current_cents = row.get("current_cents");
+            let archived = row.get("archived");
+            let created_at = row.get("created_at");
+            let updated_at = row.get("updated_at");
+
+            let item = Item {
+                uuid,
+                name,
+                comment,
+                item_type,
+                target_cents,
+                current_cents,
+                archived,
+                created_at,
+                updated_at,
+            };
+
+            items.push(item);
+        }
+
+        Ok(items)
     }
 }
