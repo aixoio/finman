2cf1657f27a5f2d8f0386afd6c19a53dccbf0620
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-08-08 21:37:31 +1000

    add base for new page

--- src/routes/Sidebar.svelte
+++ src/routes/Sidebar.svelte
@@ -25,6 +25,21 @@
             {/snippet}
             <span>home</span>
         </SidebarLink>
+        <SidebarLink href="/new">
+            {#snippet icon()}
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    class="bi bi-house w-8 h-8"
+                    viewBox="0 0 16 16"
+                >
+                    <path
+                        d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"
+                    />
+                </svg>
+            {/snippet}
+            <span>new</span>
+        </SidebarLink>
         <SidebarLink href="/license">
             {#snippet icon()}
                 <svg
