a6602307b8f2203224b53fb4b5f9e75a0b26b09a
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-08-08 15:40:29 +1000

    add base for database struct

--- src-tauri/src/database.rs
+++ src-tauri/src/database.rs
@@ -1,1 +1,25 @@
+use std::str::FromStr;
 
+use sqlx::{
+    sqlite::{SqliteConnectOptions, SqlitePoolOptions},
+    SqlitePool,
+};
+
+pub const DATABASE_FILENAME: &str = "finman_database.sqlite3";
+
+pub struct Database {
+    pool: SqlitePool,
+}
+
+impl Database {
+    pub async fn build(filename: &str) -> Result<Self, sqlx::Error> {
+        let path = format!("sqlite:{filename}");
+
+        let options = SqliteConnectOptions::from_str(&path)?.create_if_missing(true);
+        let pool = SqlitePoolOptions::new().connect_with(options).await?;
+
+        sqlx::migrate!().run(&pool).await?;
+
+        Ok(Self { pool })
+    }
+}
