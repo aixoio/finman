df2c0015c1eae6fb60c1800fc06d762e1da7e874
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-08-09 23:45:33 +1000

    add preview_target_dialog

--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -42,6 +42,9 @@
     type TargetBtnType = "-50%" | "+50%" | "Custom %";
 
     let target_dialog_percentage: number = $state(10);
+    let preview_target_dialog = $derived(
+        target + target * (target_dialog_percentage / 100),
+    );
 
     let target_dialog_element: HTMLDialogElement;
 
@@ -129,7 +132,7 @@
                         d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"
                     />
                 </svg>
-                <span>${target}</span>
+                <span>${preview_target_dialog}</span>
             </div>
         </div>
 
