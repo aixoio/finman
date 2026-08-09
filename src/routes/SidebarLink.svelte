032d18e55858386445df1cd2db687c478f16b5ce
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-08-09 10:16:50 +1000

    update color to use neutral

--- src/routes/SidebarLink.svelte
+++ src/routes/SidebarLink.svelte
@@ -20,14 +20,14 @@
 {#if page.url.pathname == href}
     <a
         {href}
-        class="flex cursor-pointer active:cursor-pointer items-center gap-2 text-base-content/90 rounded-box px-2 py-0.5 bg-base-100 hover:bg-base-content/15 active:bg-base-content/20 select-none"
+        class="flex cursor-pointer active:cursor-pointer items-center gap-2 text-neutral-content/90 rounded-box px-2 py-0.5 bg-neutral hover:bg-neutral-content/15 active:bg-neutral-content/20 select-none"
     >
         {@render content()}
     </a>
 {:else}
     <a
         {href}
-        class="flex cursor-pointer active:cursor-pointer items-center gap-2 text-base-content/90 rounded-box px-2 py-0.5 hover:bg-base-content/5 active:bg-base-content/20 select-none"
+        class="flex cursor-pointer active:cursor-pointer items-center gap-2 text-neutral-content/90 rounded-box px-2 py-0.5 hover:bg-neutral-content/5 active:bg-neutral-content/20 select-none"
     >
         {@render content()}
     </a>
