d3880a244378f6e0359c5ed76520071a63209e23
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-08-11 15:53:38 +1000

    update display in dialog to prevent long deciamls

--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -162,7 +162,7 @@
                         d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"
                     />
                 </svg>
-                <span>${preview_target_dialog}</span>
+                <span>${preview_target_dialog.toFixed(2)}</span>
             </div>
         </div>
 
@@ -230,7 +230,7 @@
             <div class="flex justify-between">
                 <span>{starting_dialog_percentage}% of ${target}</span>
                 <span>is</span>
-                <span>${preview_starting_dialog}</span>
+                <span>${preview_starting_dialog.toFixed(2)}</span>
             </div>
         </div>
 
