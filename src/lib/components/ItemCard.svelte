f42bbd9fd6b92b9864e00ffe527a5cc7a16b2f22
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-08-12 22:32:55 +1000

    update <ItemCard /> and /item/[uuid] +page

--- src/lib/components/ItemCard.svelte
+++ src/lib/components/ItemCard.svelte
@@ -5,15 +5,22 @@
     } from "$lib/core/commands";
 
     interface Props {
-        uuid: string;
+        uuid?: string;
         name: string;
         item_type: ItemType;
         target_cents: number;
         current_cents: number;
+        show_remaining?: boolean;
     }
 
-    const { uuid, name, item_type, target_cents, current_cents }: Props =
-        $props();
+    const {
+        uuid,
+        name,
+        item_type,
+        target_cents,
+        current_cents,
+        show_remaining = false,
+    }: Props = $props();
 
     const target = $derived(target_cents / 100);
     const current = $derived(current_cents / 100);
@@ -21,19 +28,29 @@
     const percentage = $derived(
         ((current_cents / target_cents) * 100).toFixed(2),
     );
+
+    const remaining = $derived(
+        Math.max(0, (target_cents - current_cents) / 100).toFixed(2),
+    );
 </script>
 
-<a
-    class="card border border-neutral shadow bg-base-100 cursor-pointer"
-    href="/item/{uuid}"
->
+{#snippet body()}
     <div class="card-body">
         <h1 class="card-title block max-w-full truncate">
             {name}
         </h1>
-        <span class="text-xs text-base-content/50"
-            >{display_format_item_type(item_type)}</span
-        >
+        <div class="flex justify-between w-full">
+            <span class="text-xs text-base-content/50"
+                >{display_format_item_type(item_type)}</span
+            >
+
+            {#if show_remaining}
+                <span
+                    class="px-3 py-0.5 bg-neutral-700 rounded-full shadow border border-neutral-content/25 text-base-content text-xs"
+                    >${remaining} left</span
+                >
+            {/if}
+        </div>
 
         <progress
             class="progress w-full h-4"
@@ -49,4 +66,17 @@
             <span class="text-xs text-base-content/65">{percentage}%</span>
         </div>
     </div>
-</a>
+{/snippet}
+
+{#if uuid}
+    <a
+        class="card border border-neutral shadow bg-base-100 cursor-pointer"
+        href="/item/{uuid}"
+    >
+        {@render body()}
+    </a>
+{:else}
+    <div class="card border border-neutral shadow bg-base-100">
+        {@render body()}
+    </div>
+{/if}
--- src/routes/item/[uuid]/+page.svelte
+++ src/routes/item/[uuid]/+page.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
+    import ItemCard from "$lib/components/ItemCard.svelte";
     import type { PageProps } from "./$types";
 
     const { data }: PageProps = $props();
 </script>
 
-<h1>item page</h1>
-<h1>{JSON.stringify(data.item)}</h1>
+<div class="xl:mx-auto xl:max-w-2/3 not-xl:m-8">
+    <ItemCard
+        name={data.item.name}
+        item_type={data.item.item_type}
+        target_cents={data.item.target_cents}
+        current_cents={data.item.current_cents}
+        show_remaining
+    ></ItemCard>
+</div>
