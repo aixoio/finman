1cbee5fc396cef550dfda92a2f5a60bf01f99b5d
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-08-15 22:17:38 +1000

    scroll on navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
+    import { afterNavigate } from "$app/navigation";
     import "./layout.css";
     import Sidebar from "./Sidebar.svelte";
 
     const { children } = $props();
+
+    let content: HTMLElement;
+
+    afterNavigate(() => {
+        content?.scrollTo({ top: 0, left: 0 });
+    });
 </script>
 
 <div class="flex h-dvh overflow-hidden">
     <Sidebar></Sidebar>
 
-    <main class="min-w-0 flex-1 overflow-y-auto overscroll-contain">
+    <main
+        bind:this={content}
+        class="min-w-0 flex-1 overflow-y-auto overscroll-contain"
+    >
         {@render children()}
     </main>
 </div>
