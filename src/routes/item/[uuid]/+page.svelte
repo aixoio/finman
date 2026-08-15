29d466417cae4672cb38e52083a719ccc852499f
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-08-15 11:17:00 +1000

    add item dialog base

--- src/routes/item/[uuid]/+page.svelte
+++ src/routes/item/[uuid]/+page.svelte
@@ -1,9 +1,48 @@
 <script lang="ts">
     import ItemCard from "$lib/components/ItemCard.svelte";
+    import type { Snippet } from "svelte";
     import type { PageProps } from "./$types";
 
     const { data }: PageProps = $props();
+
+    let item_dialog: HTMLDialogElement;
+
+    let item_dialog_cancel: () => Promise<void> = $state(async () => {});
+    let item_dialog_confirm: () => Promise<void> = $state(async () => {});
+
+    let item_dialog_content: Snippet = $state(empty);
+
+    function item_dialog_open(
+        onCancel: () => Promise<void>,
+        onConfirm: () => Promise<void>,
+        snip: Snippet,
+    ): void {
+        item_dialog_cancel = onCancel;
+        item_dialog_confirm = onConfirm;
+        item_dialog_content = snip;
+
+        item_dialog.showModal();
+    }
 </script>
+
+{#snippet empty()}
+
+{/snippet}
+
+<dialog bind:this={item_dialog} class="modal">
+    <div class="modal-box">
+        {@render item_dialog_content()}
+
+        <div class="modal-action">
+            <button class="btn btn-neutral" onclick={item_dialog_cancel}
+                >Cancel</button
+            >
+            <button class="btn btn-primary" onclick={item_dialog_confirm}
+                >Confirm</button
+            >
+        </div>
+    </div>
+</dialog>
 
 <div class="xl:mx-auto xl:max-w-2/3 not-xl:m-8">
     <ItemCard
